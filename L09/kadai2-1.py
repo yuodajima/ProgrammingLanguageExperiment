f5ac3e8c50fc14743b8ea4dc759b7e3a91538324
KENN <ken@nac.local>
2016-06-20 11:37:36 +0900

    solve kadai2-1

---
+++ L09/kadai2-1.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import scipy as sp
+import scipy.fftpack as spf
+
+from scipy.io.wavfile import write
+import matplotlib.pylab as plt
+
+# 設定
+Fs = 22050.  # サンプリング周波数 22 KHz
+fname = 'kadai2-1.wav'
+dur = 3.    
+Amp = 4000.  # 振幅 (とりあえず)
+
+delta = 1./Fs  # サンプリング間隔
+Nmax = Fs * dur  # サンプル点の数
+
+t = np.arange(Nmax) * delta
+Fcs = [262, 294, 330, 349, 392, 440, 494, 523]
+
+def getFreq(Fc, Fs, dur):
+    return Amp * np.sin( 2. * np.pi * Fc * t )
+
+
+y1 = getFreq(262, Fs, dur)
+y2 = getFreq(294, Fs, dur)
+y3 = getFreq(330, Fs, dur)
+y4 = getFreq(349, Fs, dur)
+y5 = getFreq(392, Fs, dur)
+y6 = getFreq(440, Fs, dur)
+y7 = getFreq(494, Fs, dur)                    
+y8 = getFreq(523, Fs, dur)
+
+x = np.hstack((y1, y2, y3, y4, y5, y6, y7, y8))
+write( fname, Fs, np.int16( x ).reshape(Nmax * 8, 1) )  
+    
+
+# 確認用
+
+Y = spf.fft( x )                
+fdelta = 1./(Nmax * 8 * delta)
+f = np.arange(Nmax * 8) * fdelta
+plt.figure()
+plt.subplot( 2, 1, 1 )
+plt.plot( t[:100], x[:100] )
+plt.grid
+plt.subplot( 2, 1, 2 )
+plt.semilogy( f, np.abs( Y ) )
+plt.grid()
+
+plt.show()
+
+
+
+
